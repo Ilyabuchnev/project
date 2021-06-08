7eb4dd59e851cca90968846b30111f3ed7187267
Ilyabuchnev <85159382+Ilyabuchnev@users.noreply.github.com>
2021-06-08 16:37:17 +0300

    ДЗ 1

---
+++ DZ-1-1.py
@@ -0,0 +1,11 @@
+# Задача 1
+a = 10
+print(a)
+b = 20
+print(b)
+c = a + b
+print(c)
+address1 = input('Введите адрес. Город: ')
+address2 = input('Улица: ')
+address3 = input('номер дома: ')
+print(address1 + ', ' + address2 + ', ' + address3 + '.')
